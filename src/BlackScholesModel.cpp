9f682120c2352e799b88f20f3711daf8291872e6
Mohammed Fellaji <mohammed.fellaji@ensimag.grenoble-inp.fr>
2017-09-15 11:38:20 +0200

    compile yeah

--- src/BlackScholesModel.cpp
+++ src/BlackScholesModel.cpp
@@ -7,16 +7,13 @@
 
 BlackScholesModel::BlackScholesModel(int size, double r, double rho, PnlVect *sigma, PnlVect *spot){
     size_=size;
-    this.r=r;
+    this->r_=r;
     rho_=rho;
     sigma_=sigma;
     spot_=spot;
 }
 
 void BlackScholesModel::asset(PnlMat *path, double T, int nbTimeSteps, PnlRng *rng) {
-    /* Gaussian vector*/
-    PnlVect* G=pnl_vect_create(size_);
-    pnl_vect_rng_normal(G,rng);
     /*Cholesky*/
     PnlMat* gamma=pnl_mat_create_from_scalar(size_,size_,rho_);
     PnlMat* identity=pnl_mat_create(size_,size_);
@@ -26,14 +23,14 @@ void BlackScholesModel::asset(PnlMat *path, double T, int nbTimeSteps, PnlRng *r
     pnl_mat_chol(gamma);
     PnlMat* results=pnl_mat_create(nbTimeSteps+1,size_);
     pnl_mat_set_row(path,spot_,0);
-    for (auto i = 1; i <= nbTimeSteps;i++) {
-        for (auto d=0; d < size_ ; d++) {
+    for (int i = 1; i <= nbTimeSteps;i++) {
+        for (int d=0; d < size_ ; d++) {
             double sigma_d=pnl_vect_get(sigma_,d);
             PnlVect* L_d=pnl_vect_create(size_);
             pnl_mat_get_row(L_d,gamma,d);
-            PnlVect* G_i==pnl_vect_create(size_);
+            PnlVect* G_i=pnl_vect_create(size_);
             pnl_vect_rng_normal(G_i,size_,rng);
-            double path_t_d=pnl_mat_get(path,i-1,d)*exp((r-(sigma_d*sigma_d)/2)*T/nbTimeSteps+sigma_d*sqrt(T/nbTimeSteps)*pnl_vect_scalar_prod(L_d,G_i));
+            double path_t_d=pnl_mat_get(path,i-1,d)*exp((r_-(sigma_d*sigma_d)/2)*T/nbTimeSteps+sigma_d*sqrt(T/nbTimeSteps)*pnl_vect_scalar_prod(L_d,G_i));
             pnl_mat_set(path,i,d,path_t_d);
         }
     }
