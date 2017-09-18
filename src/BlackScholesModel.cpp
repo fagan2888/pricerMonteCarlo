0a65bae98b6cfe7f7ecb9a3439494d59ab065e8f
Mohammed Fellaji <mohammed.fellaji@ensimag.grenoble-inp.fr>
2017-09-18 10:12:03 +0200

    rectifications

--- src/BlackScholesModel.cpp
+++ src/BlackScholesModel.cpp
@@ -24,12 +24,12 @@ void BlackScholesModel::asset(PnlMat *path, double T, int nbTimeSteps, PnlRng *r
     PnlMat* results=pnl_mat_create(nbTimeSteps+1,size_);
     pnl_mat_set_row(path,spot_,0);
     for (int i = 1; i <= nbTimeSteps;i++) {
+        PnlVect* G_i=pnl_vect_create(size_);
+        pnl_vect_rng_normal(G_i,size_,rng);
         for (int d=0; d < size_ ; d++) {
             double sigma_d=pnl_vect_get(sigma_,d);
             PnlVect* L_d=pnl_vect_create(size_);
             pnl_mat_get_row(L_d,gamma,d);
-            PnlVect* G_i=pnl_vect_create(size_);
-            pnl_vect_rng_normal(G_i,size_,rng);
             double path_t_d=pnl_mat_get(path,i-1,d)*exp((r_-(sigma_d*sigma_d)/2)*T/nbTimeSteps+sigma_d*sqrt(T/nbTimeSteps)*pnl_vect_scalar_prod(L_d,G_i));
             pnl_mat_set(path,i,d,path_t_d);
         }
