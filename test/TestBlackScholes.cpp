84f7bc1fb8870ad0014141adbe037b339c2a0634
Theophile Charmont <theophile.charmont@ensimag.grenoble-inp.fr>
2017-09-19 08:49:43 +0200

    modif test

--- test/TestBlackScholes.cpp
+++ test/TestBlackScholes.cpp
@@ -1,7 +1,3 @@
-//
-// Created by fellajim on 9/15/17.
-//
-
 #include "../src/BlackScholesModel.hpp"
 #include "../src/parser.hpp"
 #include <string>
@@ -13,13 +9,12 @@ int main(int argc, char **argv) {
 
     double T, r, strike, rho;
     PnlVect *spot, *sigma, *divid;
-    PnlMat* pnlMat;
+    PnlMat *pnlMat;
     string type;
-    int size,nbtt;
+    int size, nbtt;
     size_t n_samples;
 
-    char *infile = argv[1];
-    Param *P = new Parser(infile);
+    Param *P = new Parser("../data/basket.dat");
 
     P->extract("option type", type);
     P->extract("maturity", T);
@@ -27,33 +22,18 @@ int main(int argc, char **argv) {
     P->extract("spot", spot, size);
     P->extract("volatility", sigma, size);
     P->extract("interest rate", r);
-    if (P->extract("dividend rate", divid, size) == false)
-    {
+    if (P->extract("dividend rate", divid, size) == false) {
         divid = pnl_vect_create_from_zero(size);
     }
     P->extract("strike", strike);
     P->extract("sample number", n_samples);
 
-    P->print();
-    cout << endl;
-    cout << "option type " << type << endl;
-    cout << "maturity " << T << endl;
-    cout << "strike " << strike << endl;
-    cout << "option size " << size << endl;
-    cout << "interest rate " << r << endl;
-    cout << "dividend rate ";
-    pnl_vect_print_asrow(divid);
-    cout << "spot ";
-    pnl_vect_print_asrow(spot);
-    cout << "volatility ";
-    pnl_vect_print_asrow(sigma);
-    cout << "Number of samples " << n_samples << endl;
-    nbtt=11;
-    PnlRng* rng=pnl_rng_create(PNL_RNG_KNUTH);
-    pnlMat=pnl_mat_create_from_zero(nbtt+1,size);
-    BlackScholesModel blackScholesModel(size, r, rho,sigma, spot);
-    blackScholesModel.asset(pnlMat,T,nbtt,rng);
-    pnl_mat_print(pnlMat);
+    nbtt = 11;
+    PnlRng *rng = pnl_rng_create(PNL_RNG_KNUTH);
+    pnlMat = pnl_mat_create_from_zero(nbtt + 1, size);
+    BlackScholesModel blackScholesModel(size, r, rho, sigma, spot);
+    blackScholesModel.asset(pnlMat, T, nbtt, rng);
+    //pnl_mat_print(pnlMat);
     pnl_vect_free(&spot);
     pnl_vect_free(&sigma);
     pnl_vect_free(&divid);
