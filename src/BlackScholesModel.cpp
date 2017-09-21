513e03ae50536eeec67fcefba3829423b092e7cd
Theophile Charmont <theophile.charmont@ensimag.grenoble-inp.fr>
2017-09-21 11:50:13 +0200

    libérer Lacrim

--- src/BlackScholesModel.cpp
+++ src/BlackScholesModel.cpp
@@ -21,12 +21,12 @@ void BlackScholesModel::asset(PnlMat *path, double T, int nbTimeSteps, PnlRng *r
     pnl_mat_plus_mat(gamma, identity);
     pnl_mat_chol(gamma);
     pnl_mat_set_row(path, spot_, 0);
+    PnlVect *G_i = pnl_vect_create(size_);
+    PnlVect *L_d = pnl_vect_create(size_);
     for (int i = 1; i <= nbTimeSteps; i++) {
-        PnlVect *G_i = pnl_vect_create(size_);
         pnl_vect_rng_normal(G_i, size_, rng);
         for (int d = 0; d < size_; d++) {
             double sigma_d = pnl_vect_get(sigma_, d);
-            PnlVect *L_d = pnl_vect_create(size_);
             pnl_mat_get_row(L_d, gamma, d);
             double path_t_d = pnl_mat_get(path, i - 1, d) * exp((r_ - pow(sigma_d, 2) / 2) * T / nbTimeSteps +
                                                                 sigma_d * sqrt(T / nbTimeSteps) *
@@ -34,6 +34,11 @@ void BlackScholesModel::asset(PnlMat *path, double T, int nbTimeSteps, PnlRng *r
             pnl_mat_set(path, i, d, path_t_d);
         }
     }
+    pnl_mat_free(&gamma);
+    pnl_mat_free(&identity);
+    pnl_vect_free(&G_i);
+    pnl_vect_free(&L_d);
+
 }
 
 void BlackScholesModel::asset(PnlMat *path, double t, double T, int nbTimeSteps, PnlRng *rng, const PnlMat *past) {
