a71a84e825dab0c1f107bf642415dbb3a482a448
Mohammed Fellaji <mohammed.fellaji@ensimag.grenoble-inp.fr>
2017-09-25 14:36:39 +0200

    merge

--- test/TestPricerHistory.cpp
+++ test/TestPricerHistory.cpp
@@ -6,6 +6,7 @@
 #include "../src/OptionAsian.hpp"
 #include "../src/OptionBasket.hpp"
 #include "../src/OptionPerformance.hpp"
+#include "../src/HedgingPortfolio.hpp"
 
 #include "pnl/pnl_matrix.h"
 
@@ -52,8 +53,8 @@ int main(int argc, char **argv) {
 
     type = "basket";
     strike =100;
-    T = 1.0;
-    t = 0.0;
+    T = 2.0;
+    t = 1.0;
     size = 40;
     fdStep = 0.01;
     spot = pnl_vect_create_from_scalar(size, 100.0);
@@ -65,8 +66,8 @@ int main(int argc, char **argv) {
     n_samples = 1;
     nbTimeSteps = 200;
 
-    PnlMat* history = pnl_mat_create_from_scalar(1, size, 100.0);//pnl_mat_create_from_file(infile);
-    //PnlMat *history = pnl_mat_create_from_file(infile);
+    //PnlMat* history = pnl_mat_create_from_scalar(1, size, 100.0);//pnl_mat_create_from_file(infile);
+    PnlMat *history = pnl_mat_create_from_file(infile);
 
     //pnl_mat_print(history);
 
@@ -84,14 +85,17 @@ int main(int argc, char **argv) {
     PnlRng *rng = pnl_rng_create(PNL_RNG_MERSENNE);
     pnl_rng_sseed(rng, time(NULL));
 
+    std::cout << "_____ Hedging Portfolio _____"<< std::endl;
+
     BlackScholesModel* bsmod = new BlackScholesModel(size, r, corr, sigma, spot);
     MonteCarlo monteCarlo(bsmod, opt, rng, fdStep, (int) n_samples); /* n_samples */
+    HedgingPortfolio hedgingPortfolio(nbTimeSteps,&monteCarlo);
+    PnlVect* results=pnl_vect_create_from_scalar(nbTimeSteps,0);
+    hedgingPortfolio.hedgingPAndL(results,history);
+    pnl_vect_print(results);
 
-	double prix;
+    double prix;
 	double ic;
-    std::cout << "_____ MonteCarlo Computation_____"<< std::endl;
-    monteCarlo.delta(history, t, delta);
-    pnl_vect_print(delta);
     //monteCarlo.price(history, t , prix, ic);
     //monteCarlo.price(prix, ic);
     //std::cout << prix << " | " << ic << std::endl;
