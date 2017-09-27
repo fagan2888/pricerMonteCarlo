782e95efa458e2318d9d31f4193c8ea19e5858c5
Mohammed Fellaji <mohammed.fellaji@ensimag.grenoble-inp.fr>
2017-09-27 16:02:08 +0200

    [optim] Get 1 fois au lieu de deux

--- src/OptionPerformance.cpp
+++ src/OptionPerformance.cpp
@@ -13,8 +13,9 @@ double OptionPerformance::payoff(const PnlMat *path) {
         before = 0;
         now = 0;
         for (int d = 0; d < size_; d++) {
-            before += GET(payoffCoeff_,d) * MGET(path, i - 1, d);
-            now += GET(payoffCoeff_,d) * MGET(path, i, d);
+            double coefficient=GET(payoffCoeff_,d);
+            before += coefficient * MGET(path, i - 1, d);
+            now += coefficient * MGET(path, i, d);
         }
         payoff += (now / before - 1 < 0) ? 0 : now / before - 1;
     }
