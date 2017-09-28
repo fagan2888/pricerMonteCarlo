a1d9570b2b0328d34b92fc416e3e0799e6473a59
Edouard Marguerite <margueed@ensipc462.imag.fr>
2017-09-28 11:08:34 +0200

    Affichage du temps.

--- hop_pricer.sh
+++ hop_pricer.sh
@@ -2,24 +2,24 @@
 
 echo "Pricer"
 echo "Asian"
-./build/pricer ./data/asian.dat
+time ./build/pricer ./data/asian.dat
 echo "Basket"
-./build/pricer ./data/basket.dat
+time ./build/pricer ./data/basket.dat
 echo "Basket_1"
-./build/pricer ./data/basket_1.dat
+time ./build/pricer ./data/basket_1.dat
 echo "Basket_2"
-./build/pricer ./data/basket_2.dat
+time ./build/pricer ./data/basket_2.dat
 echo "Perf"
-./build/pricer ./data/perf.dat
+time ./build/pricer ./data/perf.dat
 echo ""
 echo "Pricer -c"
 echo "Asian"
-./build/pricer -c ./data/market-data/simul_asian.dat ./data/asian.dat
+time ./build/pricer -c ./data/market-data/simul_asian.dat ./data/asian.dat
 echo "Basket"
-./build/pricer -c ./data/market-data/simul_basket.dat ./data/basket.dat
+time ./build/pricer -c ./data/market-data/simul_basket.dat ./data/basket.dat
 echo "Basket_1"
-./build/pricer -c ./data/market-data/simul_basket_1.dat ./data/basket_1.dat
+time ./build/pricer -c ./data/market-data/simul_basket_1.dat ./data/basket_1.dat
 echo "Basket_2"
-./build/pricer -c ./data/market-data/simul_basket_2.dat ./data/basket_2.dat
+time ./build/pricer -c ./data/market-data/simul_basket_2.dat ./data/basket_2.dat
 echo "Perf"
-./build/pricer -c ./data/market-data/simul_perf.dat ./data/perf.dat
+time ./build/pricer -c ./data/market-data/simul_perf.dat ./data/perf.dat
